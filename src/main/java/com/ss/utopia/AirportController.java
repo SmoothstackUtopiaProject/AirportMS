5467901592b05a9dfa91cb304828e07804424ebe
JoshuaTyler-SmoothStack <Joshua.TylerSmoothStack@com>
2021-03-19 17:56:46 -0400

    Removed unused controller variables

--- src/main/java/com/ss/utopia/AirportController.java
+++ src/main/java/com/ss/utopia/AirportController.java
@@ -11,10 +11,8 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,13 +23,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping(
-  value = "/airports",
-  produces = { "application/json", "application/xml", "text/xml" },
-  consumes = MediaType.ALL_VALUE
-)
+@RequestMapping("/airports")
 public class AirportController {
 
   @Autowired
